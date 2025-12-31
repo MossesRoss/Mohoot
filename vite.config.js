10f479a5ec56a8ea25a3f241d48421e81070f616
MossesRoss <mosasross@gmail.com>
2025-12-31 18:10:51 +0530

    Fix vite config for Vercel and GH Pages

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+// Changed as per our webhost, Vercel and GitHub Pages compatibility
 export default defineConfig({
-  base: "./",
+  base: process.env.VERCEL === '1' ? '/' : '/Mohoot/',
   plugins: [react()],
   test: {
     globals: true,
